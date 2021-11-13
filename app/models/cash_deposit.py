e2044e39cd5048659dd6ac2691262deb5d011dd6
hwc9169 <hwc9169@gmail.com>
2021-11-13 18:22:34 +0900

    API 모델 개발

--- app/models/cash_deposit.py
+++ app/models/cash_deposit.py
@@ -1,6 +1,8 @@
 from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, Boolean
+from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import relationship
 
+from app.crud.utils import get_kst_now
 from app.db import Base
 
 from typing import TYPE_CHECKING
@@ -8,19 +10,21 @@ if TYPE_CHECKING:
     from .consumer import Consumer
 from .consumer import Consumer
 
+import uuid
+
 class CashDeposit(Base):
     __tablename__ = "cash_deposit"
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
 
     description = Column(String, nullable=True)
     deposit_amount = Column(Integer, nullable=False)
 
-    request_at = Column(DateTime(timezone=True), nullable=True)
+    requested_at = Column(DateTime(timezone=True), nullable=False, default=get_kst_now)
     ack_at = Column(DateTime(timezone=True), nullable=True)
     approved_at = Column(DateTime(timezone=True), nullable=True)
     due_date = Column(DateTime(timezone=True), nullable=True)
 
-    is_cancel = Column(Boolean, nullable=True)
+    is_canceled = Column(Boolean, nullable=False, default=False)
     payment_key = Column(String, nullable=True)
 
     consumer_id = Column(Integer, ForeignKey(
