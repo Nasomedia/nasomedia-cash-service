d80202c91283560abf31769be0f9ae189760e3b3
hwc9169 <hwc9169@gmail.com>
2021-11-15 22:30:00 +0900

    openapi 수정

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -6,5 +6,5 @@ from app.api.v1.endpoints import cash_deposit
 
 api_router = APIRouter()
 api_router.include_router(consumer.router, prefix="/consumers", tags=["consumer"])
-api_router.include_router(cash_usage.router, prefix="/cash/usages", tags=["cash"])
-api_router.include_router(cash_deposit.router, prefix="/cash/deposits", tags=["cash"])
+api_router.include_router(cash_usage.router, prefix="/cash/usages", tags=["cash usage"])
+api_router.include_router(cash_deposit.router, prefix="/cash/deposits", tags=["cash deposit"])
