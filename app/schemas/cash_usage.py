7a5880475d40dca15355a5f25bf44da2902df39b
hwc9169 <hwc9169@gmail.com>
2021-11-15 21:56:25 +0900

    cash usage 스키마

--- app/schemas/cash_usage.py
+++ app/schemas/cash_usage.py
@@ -1,31 +1,36 @@
 from typing import Optional
 from pydantic import BaseModel
+from datetime import datetime
 
 # Shared properties
-class ConsumerBase(BaseModel):
-    cash: Optional[int] = 0
+class CashUsageBase(BaseModel):
+    description: Optional[str]
+    usage_amount: Optional[int]
 
 # Properties to receive on creation
-class ConsumerCreate(ConsumerBase):
-    cash: int = 0
+class CashUsageCreate(CashUsageBase):
+    usage_amount: int
 
 # Properties to receive on update
-class ConsuemrUpdate(ConsumerBase):
+class CashUsageUpdate(CashUsageBase):
     pass
 
 # Properties shared by models stored in DB
-class ConsumerInDBBase(ConsumerBase):
+class CashUsageInDBBase(CashUsageBase):
     id: int
-    
-    cash: int
+
+    usage_amount: int
+    created_at: datetime
+
+    consumer_id: int
 
     class Config:
         orm_mode = True
 
 # Properties to return to client
-class Consumer(ConsumerInDBBase):
+class CashUsage(CashUsageInDBBase):
     pass
 
 # Properties properties stored in DB
-class ConsumerInDB(ConsumerInDBBase):
+class CashUsageInDB(CashUsageInDBBase):
     pass
