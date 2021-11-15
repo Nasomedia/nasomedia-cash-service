f70a7563e6e00ff0e6ee29bafba8b7defe50f26c
hwc9169 <hwc9169@gmail.com>
2021-11-15 22:26:45 +0900

    테이블명 수정

--- app/models/consumer.py
+++ app/models/consumer.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import relationship
 from app.db import Base
 
 class Consumer(Base):
-    __tablename__ = "test_model"
+    __tablename__ = "consumer"
     id = Column(Integer, primary_key=True, index=True)
 
     cash = Column(Integer, nullable=False)
