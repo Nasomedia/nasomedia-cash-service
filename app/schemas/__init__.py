db83fbe1959291e9dfd90bb1b79b0c368b977f00
hwc9169 <hwc9169@gmail.com>
2021-11-15 22:06:51 +0900

    오타 수정

--- app/schemas/__init__.py
+++ app/schemas/__init__.py
@@ -1,6 +1,6 @@
 from .user import User, UserCreate, UserInDB, UserUpdate
 from .consumer import Consumer, ConsumerCreate, ConsumerInDB, ConsumerUpdate
 from .cash_usage import CashUsage, CashUsageCreate, CashUsageInDB, CashUsageUpdate
-from .cash_deposit import CashDeposit, CashDepositCreate, CashDepositInDB, CashDepositUpdate, 
+from .cash_deposit import CashDeposit, CashDepositCreate, CashDepositInDB, CashDepositUpdate
 from .cash_deposit import CashDepositAckRequest, CashDepositAck, CashDepositCancelRequest, CashDepositCancel, CashDepositCallbackRequest, CashDepositCallback
 from .toss_payment import Payment, RefundReceiveAccount, PaymentBase
