45cf01f5d6a2841320937f25aa20824e37af6df8
hwc9169 <hwc9169@gmail.com>
2021-12-05 18:31:08 +0900

    purchase api 추가

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -1,10 +1,12 @@
 from fastapi import APIRouter
 
 from app.api.v1.endpoints import consumer
-from app.api.v1.endpoints import cash_usage
-from app.api.v1.endpoints import cash_deposit
+from app.api.v1.endpoints import usage
+from app.api.v1.endpoints import deposit
+from app.api.v1.endpoints import purchase
 
 api_router = APIRouter()
-api_router.include_router(consumer.router, prefix="/consumers", tags=["consumer"])
-api_router.include_router(cash_usage.router, prefix="/cash/usages", tags=["cash usage"])
-api_router.include_router(cash_deposit.router, prefix="/cash/deposits", tags=["cash deposit"])
+api_router.include_router(consumer.router, prefix="/cash/consumers", tags=["consumer"])
+api_router.include_router(usage.router, prefix="/cash/usages", tags=["usage"])
+api_router.include_router(deposit.router, prefix="/cash/deposits", tags=["deposit"])
+api_router.include_router(purchase.router, prefix="/cash/purchase", tags=["purchase"])
---
+++ app/api/v1/endpoints/purchase.py
@@ -0,0 +1,39 @@
+from fastapi import APIRouter, Depends, HTTPException
+from fastapi.params import Query
+from sqlalchemy.orm import Session
+from typing import Dict, Any
+
+
+from app import crud, schemas, models, deps
+
+router = APIRouter()
+
+
+@router.get("/{episode_id}", response_model=schemas.Episode)
+async def purchase_episode(
+    db: Session = Depends(deps.get_db),
+    current_user: Dict[str, Any] = Depends(deps.get_current_active_user),
+    main_service: deps.MainService = Depends(),
+    library_service: deps.LibraryService = Depends(),
+    *,
+    episode_id: int
+):
+    """
+    Purchase episode
+    """
+    episode = await main_service.get_episode(episode_id=episode_id)
+    consumer = crud.consumer.get(db, id=current_user['user'].id)
+    if consumer.cash < episode.price:
+        raise HTTPException(status_code=400, detail='Insufficient cache')
+    await library_service.create_purchased_episode(
+        episode_id=episode_id, 
+        token=current_user['token']
+    )
+    current_cash = consumer.cash-episode.price
+    crud.consumer.update(
+        db, 
+        db_obj=consumer, 
+        obj_in=schemas.ConsumerUpdate(cash=current_cash)
+    )
+
+    return episode
